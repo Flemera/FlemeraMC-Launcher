b3fa2a6c2d4290be702ec0506fad3af97db4c2a9
Noah Bleau <nbleau@hexoraw.com>
2025-12-23 14:33:51 -0500

    Updated default values to Flemera

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = 'd1f49f56-198a-457d-9408-a362cb18106a'
 // SEE NOTE ABOVE.
 
 
